bcc9179d46bf702c5af753a68d3758118bb7ba37
jjhmero8659 <wkd86591@naver.com>
2024-07-06 17:40:01 +0900

    JUnit Bank 2024-07-06 Update

--- src/test/java/shop/mtcoding/bank/config/jwt/JwtProcessTest.java
+++ src/test/java/shop/mtcoding/bank/config/jwt/JwtProcessTest.java
@@ -1,2 +1,44 @@
-package shop.mtcoding.bank.config.jwt;public class JwtProcessTest {
+package shop.mtcoding.bank.config.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.annotation.Rollback;
+import shop.mtcoding.bank.config.auth.LoginUser;
+import shop.mtcoding.bank.domain.user.User;
+import shop.mtcoding.bank.domain.user.UserEnum;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class JwtProcessTest {
+    @Test
+    public void create_test() throws Exception{
+        // given
+        User user = User.builder()
+                .id(1L)
+                .role(UserEnum.CUSTOMER)
+                .build();
+
+        LoginUser loginUser = new LoginUser(user);
+
+        // when
+        String jwtToken = JwtProcess.create(loginUser);
+        System.out.println("테스트 : " + jwtToken);
+
+        // then
+        assertTrue(jwtToken.startsWith(JwtVO.TOKEN_PREFIX));
+    }
+
+    @Test
+    public void verify_test() throws Exception{
+        // given
+        String jwtToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJiYW5rIiwicm9sZSI6IkNVU1RPTUVSIiwiaWQiOjEsImV4cCI6NzM5MjQ1MDE5Nzk4MDMwOX0.dQMouDeS3xggb-wjDoAm6b2_mOHY0ulfo4Qn66vC1wVLY8EpS2lVN6-i5Y3DYIB5loKPtV7yVQOBStgGjGtCDw";
+
+        // when
+        LoginUser loginUser = JwtProcess.verify(jwtToken);
+        System.out.println("테스트 : " + loginUser.getUser().getId());
+        System.out.println("테스트 : " + loginUser.getUser().getRole());
+
+        // then
+        assertThat(loginUser.getUser().getId()).isEqualTo(1L);
+    }
 }
